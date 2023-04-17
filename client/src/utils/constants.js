b15db2c1f4e031f33d7beaa3b53df0a69e3709f4
Rayyan Anugerah <rayyananugerah2001@gmail.com>
2023-04-17 17:58:02 +0800

    first

---
+++ client/src/utils/constants.js
@@ -0,0 +1,10 @@
+import abi from './Transactions.json';
+import aji from './File.json'
+
+
+export const contractABI = abi.abi;
+export const fileABI = aji.abi;
+export const contractAddress = '0xF0169fb5da6833d0DF6b255C27691Bc4c34dE0C3';
+export const fileAddress = '0xc0350C628E94176F7d14E67249a5f3E2205F12Ff';
+export const imageAddress= '0x9306811f9e72354BE07cF5Dd5C27fA407C679a92';
+export const imageABI = aji.abi;
